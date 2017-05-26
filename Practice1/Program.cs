96906f2a9188b090e960abcfa2bce83eb55a40be
IskusnikXD <IskusnikXD@gmail.com>
2017-05-26 23:01:04 +0500

    Так без ошибки компиляции. тест 10 падает

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,53 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-namespace Practice1
-{
-    class Program
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            //in N чисел
-            //4 n       пример
-            //1 n1
-            // 2 n2
-            // 4 n3
-            //8 n4      ответ 16 
-            //out минимальное число не представимое их суммой, каждое число не более одного раза
-            int N; 
             
-            FileStream input = new FileStream("INPUT.TXT", FileMode.OpenOrCreate, FileAccess.Read);
-            StreamReader eye = new StreamReader(input);
-            FileStream output = new FileStream("OUTPUT.TXT", FileMode.Create, FileAccess.Write);
+            int N;
 
-            N = int.Parse(eye.ReadLine());
+            StreamReader input = new StreamReader("input.txt");
+            StreamWriter output = new StreamWriter("output.txt");
+
+            N = int.Parse(input.ReadLine());
             int[] a = new int[N];
             int min = -1;
             int sum = 0;
             for (int i = 0; i < N; i++)
-                a[i] = int.Parse(eye.ReadLine());
+                a[i] = int.Parse(input.ReadLine());
 
             Array.Sort(a);
 
             for (int i = 0; i < a.Length; i++)
             {
-                if ((a[i] - sum > 1) && (min == -1)) //Берём наименьшее число
-                    min = sum + 1;                   //Если разница между следующим элементом 
-                                                     //и суммой предыдущих больше 1, 
-                                                     //то сумма+1 - искомое число 
-                                                     //(Если 1, то можно покрыть все числа до суммы(предыдущих с этим) - почему ?)
-                                                     //Типа предыдущие обеспечиваются на предыдущих этапах
-                                                     //на случай плохой памяти. Читать проще.
+                if ((a[i] - sum > 1) && (min == -1)) 
+                    min = sum + 1;                   
+
                 sum += a[i];
             }
             if (min == -1)
                 min = sum + 1;
 
-            Console.WriteLine(min);
-            //Надо потестировать
+            output.WriteLine(min);
+
+            input.Close();
+            output.Close();
+
         }
     }
-}
+
