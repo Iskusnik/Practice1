e7f2b5de1837ca4dc3be41724dbeac098c1696c1
IskusnikXD <IskusnikXD@gmail.com>
2017-06-11 22:34:55 +0500

    Набросок сортировки Шелла. Необходимо тестирование

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,40 +1,68 @@
 ﻿using System;
 using System.IO;
 
-    public class Program
+public class Program
+{
+
+    static void MyShellSort(ref Int64[] arr)
     {
-        static void Main(string[] args)
+        int distance = arr.Length / 2;
+        int i = 0;
+        int j = 0;
+        Int64 swap = 0;
+
+        while (distance > 1)
         {
-            
-            Int64 N;
-
-            StreamReader input = new StreamReader("input.txt");
-            StreamWriter output = new StreamWriter("output.txt");
-
-            N = Int64.Parse(input.ReadLine());
-            Int64[] a = new Int64[N];
-            Int64 min = -1;
-            Int64 sum = 0;
-            for (int i = 0; i < N; i++)
-                a[i] = Int64.Parse(input.ReadLine());
-
-            Array.Sort(a);
-
-            for (int i = 0; i < a.Length; i++)
+            while (i < distance)
             {
-                if ((a[i] - sum > 1) && (min == -1)) 
-                    min = sum + 1;                   
-
-                sum += a[i];
+                j = i;
+                while (j < arr.Length)
+                {
+                    if (arr[j] > arr[j + distance])
+                    {
+                        swap = arr[j];
+                        arr[j] = arr[j + distance];
+                        arr[j + distance] = swap;
+                    }
+                    j += distance;
+                }
+                i++;
             }
-            if (min == -1)
-                min = sum + 1;
-
-            output.WriteLine(min);
-
-            input.Close();
-            output.Close();
-
         }
     }
+
+    static void Main(string[] args)
+    {
+
+        Int64 N;
+
+        StreamReader input = new StreamReader("input.txt");
+        StreamWriter output = new StreamWriter("output.txt");
+
+        N = Int64.Parse(input.ReadLine());
+        Int64[] a = new Int64[N];
+        Int64 min = -1;
+        Int64 sum = 0;
+        for (int i = 0; i < N; i++)
+            a[i] = Int64.Parse(input.ReadLine());
+
+        MyShellSort(ref a);
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            if ((a[i] - sum > 1) && (min == -1))
+                min = sum + 1;
+
+            sum += a[i];
+        }
+        if (min == -1)
+            min = sum + 1;
+
+        output.WriteLine(min);
+
+        input.Close();
+        output.Close();
+
+    }
+}
 
