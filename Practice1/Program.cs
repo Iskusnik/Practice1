9939980f863b9f521e25164fd9b09615a2264e43
IskusnikXD <IskusnikXD@gmail.com>
2017-06-12 08:57:07 +0500

    Для тестирования сортировки Шелла

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -6,63 +6,65 @@ public class Program
 
     static void MyShellSort(ref Int64[] arr)
     {
-        int distance = arr.Length / 2;
         int i = 0;
         int j = 0;
         Int64 swap = 0;
 
-        while (distance > 1)
-        {
-            while (i < distance)
+        for (int distance = arr.Length / 2; distance > 0; distance /= 2)    //Задаём шаг
+            for (i = distance; i < arr.Length; i++)                         //Задаём группу для сортировки вставками:
             {
-                j = i;
-                while (j < arr.Length)
+                swap = arr[i];                                              //Сортировка встаавками
+                for (j = i; j >= distance; j -= distance)                   
                 {
-                    if (arr[j] > arr[j + distance])
-                    {
-                        swap = arr[j];
-                        arr[j] = arr[j + distance];
-                        arr[j + distance] = swap;
-                    }
-                    j += distance;
+                    if (swap < arr[j - distance])                           
+                        arr[j] = arr[j - distance]; 
+                    else
+                        break;
                 }
-                i++;
+                arr[j] = swap;
             }
-        }
     }
 
     static void Main(string[] args)
     {
 
         Int64 N;
+        /*
+                StreamReader input = new StreamReader("input.txt");
+                StreamWriter output = new StreamWriter("output.txt");
 
-        StreamReader input = new StreamReader("input.txt");
-        StreamWriter output = new StreamWriter("output.txt");
+                N = Int64.Parse(input.ReadLine());
+                Int64[] a = new Int64[N];
+                Int64 min = -1;
+                Int64 sum = 0;
+                for (int i = 0; i < N; i++)
+                    a[i] = Int64.Parse(input.ReadLine());
 
-        N = Int64.Parse(input.ReadLine());
-        Int64[] a = new Int64[N];
-        Int64 min = -1;
-        Int64 sum = 0;
-        for (int i = 0; i < N; i++)
-            a[i] = Int64.Parse(input.ReadLine());
+                MyShellSort(ref a);
 
-        MyShellSort(ref a);
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if ((a[i] - sum > 1) && (min == -1))
+                        min = sum + 1;
 
-        for (int i = 0; i < a.Length; i++)
-        {
-            if ((a[i] - sum > 1) && (min == -1))
-                min = sum + 1;
+                    sum += a[i];
+                }
+                if (min == -1)
+                    min = sum + 1;
 
-            sum += a[i];
-        }
-        if (min == -1)
-            min = sum + 1;
+                output.WriteLine(min);
 
-        output.WriteLine(min);
-
-        input.Close();
-        output.Close();
-
+                input.Close();
+                output.Close();*/
+        N = Int64.Parse(Console.ReadLine());
+        Int64[] arr = new Int64[N];
+        for (int i = 0; i < arr.Length; i++) 
+            arr[i] = Int64.Parse(Console.ReadLine());
+        MyShellSort(ref arr);
+        Console.WriteLine();
+        for (int i = 0; i < arr.Length; i++)
+            Console.Write(arr[i] + " ");
+        Console.Read();
     }
 }
 
